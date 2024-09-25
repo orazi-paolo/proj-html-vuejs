6f4c33d03eeac4e9daf6ed2a01c72f57ef477372
orazi-paolo <orazi.paolo10@gmail.com>
2024-09-25 12:55:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use "/node_modules/bootstrap/scss/bootstrap.scss"
+</style>
