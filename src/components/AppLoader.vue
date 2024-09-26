ac46d417b4e563c70ec23f5b4cb7ace098e67456
orazi-paolo <orazi.paolo10@gmail.com>
2024-09-27 00:59:43 +0200

    fix loader

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -1,23 +1,23 @@
 <script>
 export default {
-  data() {
-    return {
-      
-    }
-  }
-}
+  name: 'AppLoader',
+};
 </script>
 
 <template>
-    <div class="bg-loader d-flex justify-content-center align-items-center">
-        <img src="../assets/preloader.gif" alt="Loader">
-    </div>
+  <div class="bg-loader d-flex justify-content-center align-items-center">
+    <img src="../assets/preloader.gif" alt="Loader">
+  </div>
 </template>
 
 <style lang="scss"scoped>
-.bg-loader{
-    width: 100vw;
-    height: 100vh;
-    background-color: #1c263d;
+.bg-loader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #1c263d;
+  z-index: 9999;
 }
 </style>
