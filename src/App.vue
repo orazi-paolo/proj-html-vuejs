a7600828c6db203659b21e0242f36175c1112fa1
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-09-26 22:52:00 +0200

    modify color scrollbar page

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ new WOW().init()
 export default {
   data() {
     return {
-      
+
     }
   },
   components: {
@@ -33,7 +33,23 @@ export default {
 @use "/node_modules/bootstrap/scss/bootstrap.scss";
 @use "/src/style/general.scss";
 @use '/node_modules/bootstrap-icons/font/bootstrap-icons.min.css';
-.pointer{
+
+.pointer {
   cursor: pointer;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #00013C;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #5935C2;
 }
 </style>
