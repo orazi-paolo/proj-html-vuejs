1b821a0b99fdf0a855fcef20aeeab36295b9f2b3
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-26 11:59:50 +0200

    added background and imported BaseButton on AppMainFeatureBattle

--- src/components/appMain-feature/AppMainFeatureBattle.vue
+++ src/components/appMain-feature/AppMainFeatureBattle.vue
@@ -1,6 +1,7 @@
 <script>
 import AppMainFeatureBattleCardList from './featureBattle/FeatureBattleCardList.vue'
 import AppMainFeatureBattleCardListItem from './featureBattle/BattleCardListItem.vue'
+import BaseSectionTitle from './../baseComponents/BaseSectionTitle.vue'
 export default {
   data() {
     return {
@@ -8,20 +9,33 @@ export default {
     }
   },
   components: {
+    BaseSectionTitle,
     AppMainFeatureBattleCardList,
-    AppMainFeatureBattleCardListItem
-
+    AppMainFeatureBattleCardListItem,
   }
 }
 </script>
 
 <template>
-  <h3>
-    feature battle
-  </h3>
-  <AppMainFeatureBattleCardList />
-  <AppMainFeatureBattleCardListItem />
+  <div class="background">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <BaseSectionTitle 
+            contentGreen="Trending Battle"
+            mainTitle="Keep Eyes & Manage upcoming Battle"
+            description="libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta."
+            :isCenter=true
+          />
+        </div>
+      </div>
+      <AppMainFeatureBattleCardList />
+      <AppMainFeatureBattleCardListItem />
+      
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+
 </style>
