0e2b335c6f790921c3040a6d0fc7cc3de97b9d97
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-26 11:12:12 +0200

    added getImagePathé() and style on card-gaming of AppMainFeatureMakeYourMarkCardListItem

--- src/components/appMain-feature/featureMakeYourMark/AppMainFeatureMakeYourMarkCardListItem.vue
+++ src/components/appMain-feature/featureMakeYourMark/AppMainFeatureMakeYourMarkCardListItem.vue
@@ -15,18 +15,38 @@ export default {
 
   },
   methods: {
-
+    getImagePath(img){
+        return new URL(`../../../assets/icon/${img}`, import.meta.url).href
+    }
   },
 }
 </script>
 
 <!-- AppMainFeatureMakeYourMarkCardListItem -->
 <template>
-    <li>
-
+    <li class="card-gaming">
+        <div class="box-icon">
+            <img :src="getImagePath(item.imgUrl)" :alt="item.imgUrl">
+        </div>
+        <div class="box-info">
+            <h3>{{ item.title }}</h3>
+            <p>{{ item.description }}</p>
+        </div>
     </li>
 </template>
 
 <style lang="scss" scoped>
+    .card-gaming{
+        display: flex;
+        gap: 10px;
+        
+        
+        .box-icon{
+            flex-basis: 20%;
+        }
+        .box-info{
+            flex-grow: 1;
+        }
+    }
 
 </style>
