0f861ccf5b610c347bce266e1b76f6cfa701a930
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-26 14:45:44 +0200

    added getIconPath(), imgUrlYoutube and imgUrlTwitter on BattleCardListItem

--- src/components/appMain-feature/featureBattle/BattleCardListItem.vue
+++ src/components/appMain-feature/featureBattle/BattleCardListItem.vue
@@ -2,7 +2,9 @@
 export default {
   data() {
     return {
-      imgUrlVersus:'verus-icon.png'
+      imgUrlVersus:'verus-icon.png',
+      imgUrlYoutube:'youtube-icon.png',
+      imgUrlTwitter:'twitch-icon.png',
     }
   },
   props:{
@@ -14,6 +16,9 @@ export default {
   methods:{
     getImagePath(img){
         return new URL(`../../../assets/${img}`, import.meta.url).href
+    },
+    getIconPath(icon){
+        return new URL(`../../../assets/icon/${icon}`, import.meta.url).href
     }
   }
 }
@@ -21,14 +26,27 @@ export default {
 
 <template>
   <li class="card-battle">
-    <img :src="getImagePath(battle.imgUrlTeamOne)" :alt="battle.imgUrlTeamOne">
-    <img :src="getImagePath(imgUrlVersus)" :alt="imgUrlVersus">
-    <img :src="getImagePath(battle.imgUrlTeamTwo)" :alt="battle.imgUrlTeamOne">
+    <div class="battle">
+      <img :src="getImagePath(battle.imgUrlTeamOne)" :alt="battle.imgUrlTeamOne">
+      <img :src="getImagePath(imgUrlVersus)" :alt="imgUrlVersus">
+      <img :src="getImagePath(battle.imgUrlTeamTwo)" :alt="battle.imgUrlTeamOne">
+    </div>
+    <div class="info">
+      <h4>{{ battle.title }}</h4>
+      <p>December 25, 2022 4:00 PM</p>
+    </div>
+    <div class="links">
+      <img :src="getIconPath(imgUrlYoutube)" :alt="imgUrlYoutube">
+      <span>Youtube</span>
+      <img :src="getIconPath(imgUrlTwitter)" :alt="imgUrlTwitter">
+      <span>Twitch</span>
+    </div>
   </li>
 </template>
 
 <style scoped>
 .card-battle{
   background-color: #fff;
+  flex-basis: calc(50% - 20px/2);
 }
 </style>
