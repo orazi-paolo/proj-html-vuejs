3e749aa0f149d39299b7775f5385b44e7acb168e
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-26 12:25:25 +0200

    added listBattles[] on AppMainFeatureBattleCardList

--- src/components/appMain-feature/featureBattle/FeatureBattleCardList.vue
+++ src/components/appMain-feature/featureBattle/FeatureBattleCardList.vue
@@ -2,7 +2,28 @@
 export default {
   data() {
     return {
-      
+      listBattles: [
+        {
+          imgUrlTeamOne:'game-img-1.png',
+          imgUrlTeamTwo:'game-img-2.png',
+          title:'Call Of Deauty',
+        },
+        {
+          imgUrlTeamOne:'game-img-3.png',
+          imgUrlTeamTwo:'game-img-4.png',
+          title:'Legendary Battles',
+        },
+        {
+          imgUrlTeamOne:'game-img-5.png',
+          imgUrlTeamTwo:'game-img-6.png',
+          title:'Nay Corral Gundown',
+        },
+        {
+          imgUrlTeamOne:'game-img-7.png',
+          imgUrlTeamTwo:'game-img-8.png',
+          title:'League of Legends',
+        },
+      ]
     }
   }
 }
