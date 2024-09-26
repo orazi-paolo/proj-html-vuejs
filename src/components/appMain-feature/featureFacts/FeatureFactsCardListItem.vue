0ed7924e8ec87aced3bb5539f2808128d7e9c9bc
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-26 21:09:53 +0200

    added comments on FeatureFactsCardList

--- src/components/appMain-feature/featureFacts/FeatureFactsCardListItem.vue
+++ src/components/appMain-feature/featureFacts/FeatureFactsCardListItem.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      
+      incrementedNumber: 0,
     }
   },
   props:{
@@ -10,16 +10,45 @@ export default {
       required: true,
       type: Object,
     }
+  },
+  methods:{
+    // function that generates a random number from min to max
+    getRandomNumber(min = 0, max){
+      // generate the random number
+      let result = Math.floor(Math.random() * (max + 1 -min)) + min;
+      return result;
+    },
+
+    getIncreaseNumber(){
+      // call getRandomNumber
+      this.incrementedNumber = this.getRandomNumber(1, 10); // esempio con min 1 e max 10
+
+      // start incrementing the number every 50ms
+      const increaseNumber = setInterval(() => {
+        this.incrementedNumber++; // Aggiorno il numero reattivamente
+      }, 50);
+
+      // after 1.5 seconds, the increment stops
+      setTimeout(() => {
+        clearInterval(increaseNumber);
+      }, 1500);
+    }
+  },
+  mounted(){
+    this.getIncreaseNumber()
   }
 }
 </script>
 
 <!-- FeatureFactsCardListItem -->
 <template>
-<li class="card-fact">
-  <h5>numb</h5>
-  <p>{{ fact.title }}</p>
-</li>
+  <!-- li of the single card-fact(fact) -->
+  <li class="card-fact">
+    <!-- number of the fact that increases by the method: getIncreaseNumber() -->
+    <h5>{{ incrementedNumber  }}</h5>
+    <!-- title of the card -->
+    <p>{{ fact.title }}</p>
+  </li>
 </template>
 
 <style lang="scss" scoped>
