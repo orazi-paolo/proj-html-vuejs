77927962f3f4c9108710ceac2c7c5f8c7940c6ed
orazi-paolo <orazi.paolo10@gmail.com>
2024-09-26 17:01:28 +0200

    fix baseSectionTitle and update general code

--- src/components/appMain-feature/AppMainFeatureBattle.vue
+++ src/components/appMain-feature/AppMainFeatureBattle.vue
@@ -20,23 +20,17 @@ export default {
 <!-- FeatureBattle -->
 <template>
   <section class="battle-background">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-6">
           <BaseSectionTitle 
             contentGreen="Trending Battle"
             mainTitle="Keep Eyes & Manage upcoming Battle"
             description="libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta."
-            :isCenter=true
+            :isCenter='true'
           />
-        </div>
-      </div>
       <AppMainFeatureBattleCardList /> 
       <div class="text-center">
         <!-- button imported from BaseComponents -->
         <BaseButton contentButton="VIEW ALL MATCH"/>
       </div>
-    </div>
   </section>
 </template>
 
