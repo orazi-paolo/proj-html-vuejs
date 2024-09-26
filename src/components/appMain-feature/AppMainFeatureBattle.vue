7f7a0296764de20bb7e735e7342fb0a4601c9052
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-26 10:09:45 +0200

    fixed and moved template, style and BaseSectionTitle of AppMainFeatureBattle on AppMainFeatureMakeYourMark

--- src/components/appMain-feature/AppMainFeatureBattle.vue
+++ src/components/appMain-feature/AppMainFeatureBattle.vue
@@ -1,7 +1,6 @@
 <script>
 import AppMainFeatureBattleCardList from './featureBattle/FeatureBattleCardList.vue'
 import AppMainFeatureBattleCardListItem from './featureBattle/BattleCardListItem.vue'
-import BaseSectionTitle from './../baseComponents/BaseSectionTitle.vue'
 export default {
   data() {
     return {
@@ -10,39 +9,19 @@ export default {
   },
   components: {
     AppMainFeatureBattleCardList,
-    AppMainFeatureBattleCardListItem,
-    BaseSectionTitle,
+    AppMainFeatureBattleCardListItem
+
   }
 }
 </script>
 
 <template>
-  <div class="container">
-    <div class="row-battle">
-      <div class="col-battle-left"></div>
-      <div class="col-battle-right">
-        <BaseSectionTitle 
-          contentGreen="The Ultimate Competitive Gaming Platfrom" 
-          mainTitle="Make Your Mark on the Battle Field"
-          description="libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta."
-        />
-        <AppMainFeatureBattleCardList />
-        <AppMainFeatureBattleCardListItem />
-      </div>
-    </div>
-  </div>
+  <h3>
+    feature battle
+  </h3>
+  <AppMainFeatureBattleCardList />
+  <AppMainFeatureBattleCardListItem />
 </template>
 
-<style lang="scss">
-  .row-battle{
-    display: flex;
-    justify-content: space-between;
-
-    .col-battle-left{
-    flex-basis: 40%;
-    }
-    .col-battle-right{
-    flex-basis: 60%;
-    }
-  }
+<style scoped>
 </style>
--- src/components/appMain-feature/AppMainFeatureMakeYourMark.vue
+++ src/components/appMain-feature/AppMainFeatureMakeYourMark.vue
@@ -1,19 +1,44 @@
 <script>
+import BaseSectionTitle from './../baseComponents/BaseSectionTitle.vue'
+
 export default {
   data() {
     return {
       
     }
+  },
+  components:{
+    BaseSectionTitle,
+
   }
 }
 </script>
 
 <template>
-  <h3>
-    Make YourMark
-  </h3>
-
+  <div class="container">
+    <div class="row-battle">
+      <div class="col-battle-left"></div>
+      <div class="col-battle-right">
+        <BaseSectionTitle 
+          contentGreen="The Ultimate Competitive Gaming Platfrom" 
+          mainTitle="Make Your Mark on the Battle Field"
+          description="libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta."
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+  .row-battle{
+    display: flex;
+    justify-content: space-between;
+
+    .col-battle-left{
+    flex-basis: 40%;
+    }
+    .col-battle-right{
+    flex-basis: 60%;
+    }
+  }
 </style>
